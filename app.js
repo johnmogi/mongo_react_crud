de1c8bd631039cb06918cef7f50d438325708ba4
johnmogi <anguru@gmail.com>
2021-05-04 11:05:38 +0300

    --magic

---
+++ app.js
@@ -0,0 +1,11 @@
+require("./data/dal");
+const express = require("express");
+const cors = require("cors");
+const postsController = require("./controllers/post");
+const catsController = require("./controllers//category");
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use("/api/posts", postsController);
+server.use("/api/cats", catsController);
+server.listen(3000, () => console.log("Listening on http://localhost:3000"));
